6fe3a0dbc2bcfb0b2e299e06ae199ce9ee574816
Mike Harris <mikeharris100@gmail.com>
2013-11-26 10:38:23 +0000

    Add answer explanations to science or fiction.

--- media/js/sof.js
+++ media/js/sof.js
@@ -9,6 +9,12 @@ String.prototype.format = function() {
 
     $(function() {
 
+        $('body').on('click', '.sof-answer-card', function(e) {
+            e.preventDefault();
+            $(this).parent('.sof-answer-box').toggleClass('revealed');
+            return false;
+        });
+
         var gSheetId = '0Ah6N8zTBBmsmdE02ZXM2NHpNTmFZYWkydXdhYV9Jb3c',
             sofTemplate = $('#sof-template').html(),
             insertAfter = $('h2');
@@ -20,7 +26,8 @@ String.prototype.format = function() {
             rows.splice(0, 2);
 
             rows.map(function(row){
-                var sofAnswer = $(sofTemplate.format(row.A, row.B, row.C));
+                var sofAnswer = $(sofTemplate.format(row.A, row.B, row.C,
+                                                     row.D));
                 insertAfter.after(sofAnswer);
                 insertAfter = sofAnswer;
             });
