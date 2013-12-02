8ebf8d0944e9b80ce0e58e3f22a90c0eb0c891d5
Mike Harris <mikeharris100@gmail.com>
2013-12-02 11:06:28 +0000

    Add current Tweets for Q and A.

---
+++ media/js/qanda.js
@@ -0,0 +1,68 @@
+String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+        return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+};
+
+(function($) {
+
+    $(function() {
+
+        $('body').on('click', '.tweet', function(e) {
+            e.preventDefault();
+            $(this).slideUp(function(){
+                this.remove();
+            });
+            return false;
+        });
+
+        var userKey = 'TmBELpRE1fSw',
+            tweetTemplate = $('#tweet-template').html(),
+            body = $('body'),
+            displayedTweets = {};
+
+        tweetStream(userKey, function(newTweets, currentTweets) {
+
+            var timeStamp = new Date().getTime();
+
+            $.each(currentTweets, function(i, tweet){
+
+                if(typeof(displayedTweets[tweet.id]) === 'undefined') {
+
+                    tweetHtml = $(tweetTemplate.format(
+                            tweet.id,
+                            tweet.profile_image_url, 
+                            tweet.username,
+                            tweet.text));
+
+                    displayedTweets[tweet.id] = {
+                        'displayTime': timeStamp,
+                        'obj': tweetHtml
+                    };
+
+                    tweetHtml.hide();
+                    body.append(tweetHtml);
+                    tweetHtml.slideDown();
+
+                } else {
+
+                    displayedTweets[tweet.id].displayTime = timeStamp;
+
+                }
+
+            });
+
+            $.each(displayedTweets, function(i, tweet) {
+                if(tweet.displayTime !== timeStamp){
+                    delete displayedTweets[i];    
+                    tweet.obj.slideUp(function(){
+                        this.remove();
+                    });
+                }
+            });
+        
+        }, 3000);
+
+    });
+})(jQuery);
