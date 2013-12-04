a7deff2759047b9a0aabf5007191244f533bea53
Mike Harris <mikeharris100@gmail.com>
2013-12-04 12:38:09 +0000

    Add Google Analytics.

--- media/js/common.js
+++ media/js/common.js
@@ -4,3 +4,12 @@ String.prototype.format = function() {
         return typeof args[number] != 'undefined' ? args[number] : match;
     });
 };
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-46200679-2', 'norwichsitp.github.io');
+ga('send', 'pageview');
