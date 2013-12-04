8da737a0176e43dedadf35cc622ccd45a2cf8009
Mike Harris <mikeharris100@gmail.com>
2013-12-04 11:20:41 +0000

    Move String.format into common JS file.

--- media/js/qanda.js
+++ media/js/qanda.js
@@ -1,10 +1,3 @@
-String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-        return typeof args[number] != 'undefined' ? args[number] : match;
-    });
-};
-
 (function($) {
 
     $(function() {
